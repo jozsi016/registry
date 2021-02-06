8a2c264d888707d2464f8a426ed41d78a9e028bd
Jozsef Nagy <jozsefnagy141@gmail.com>
2021-02-06 13:27:48 +0100

    Tidy up test cases

--- registry/src/test/java/hu/jnagy/registry/repository/PopulatorTest.java
+++ registry/src/test/java/hu/jnagy/registry/repository/PopulatorTest.java
@@ -12,34 +12,36 @@ import org.mockito.Spy;
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.spy;
 
 public class PopulatorTest {
 
     @Test
     public void whenPopulateRegistryWillBeSetWithPerson() {
         //Given
-        Person p1 = new Person("Manager", "Brandon", "Ulrich", Gender.MALE, LocalDate.of(1975, 04, 29));
-        Registry registry = Mockito.mock(Registry.class);
+        Person p1 = new Person("Manager", "Norbert", "Ulrich", Gender.MALE, LocalDate.of(1975, 04, 29));
+        Registry registry = new Registry ();
+        Registry spyRegistry = spy(registry);
         Populator systemUnderTest = new Populator();
         ArgumentCaptor<Person> captor = ArgumentCaptor.forClass(Person.class);
         //When
-        systemUnderTest.populate(registry);
+        systemUnderTest.populate(spyRegistry);
         //then
-        Mockito.verify(registry, Mockito.times(11)).addPerson(captor.capture());
-        assertThat(registry.getPersons().get(0), Is.is(p1));
+        Mockito.verify(spyRegistry, Mockito.times(11)).addPerson(captor.capture());
+        assertThat(spyRegistry.getPersons().get(0), Is.is(p1));
     }
 
     @Test
     public void whenPopulateRegistryWillBeSetWithPersonSpy() {
         //Given
-        Person p1 = new Person("Manager", "Brandon", "Ulrich", Gender.MALE, LocalDate.of(1975, 04, 29));
+        Person p1 = new Person("Manager", "Norbert", "Ulrich", Gender.MALE, LocalDate.of(1975, 04, 29));
         Registry registry = new Registry();
-        Registry registrySpy = Mockito.spy(registry);
+        Registry spyRegistry = spy(registry);
         Populator systemUnderTest =  new Populator();
         //When
-        systemUnderTest.populate(registrySpy);
+        systemUnderTest.populate(spyRegistry);
         //then
-        Mockito.verify(registrySpy, Mockito.times(11)).addPerson(Mockito.any());
+        Mockito.verify(spyRegistry, Mockito.times(11)).addPerson(Mockito.any());
         assertThat(registry.getPersons().get(0), Is.is(p1));
     }
 }
--- registry/src/test/java/hu/jnagy/registry/service/RegistryTest.java
+++ registry/src/test/java/hu/jnagy/registry/service/RegistryTest.java
@@ -18,52 +18,54 @@ public class RegistryTest {
     Registry registry;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         registry = new Registry();
         Populator.populate(registry);
     }
 
     @Test
     public void shouldGiveBackEveryPerson() {
-    //Given
-     Map<Integer, Person> systemUnderTest = registry.getPersons();
-    //When
-     Person p1 = systemUnderTest.get(10);
-     int numberOfPerson = systemUnderTest.size();
-     //Then
-     assertThat(p1.getFirstName(),is("Brandon"));
-     assertThat(numberOfPerson,is(11));
+        //Given
+        Map<Integer, Person> systemUnderTest = registry.getPersons();
+        Person expected = new Person("Developer", "Brandon", "Orbán", Gender.MALE, LocalDate.of(1992, 10, 02));
+        //When
+        Person actual = systemUnderTest.get(10);
+        int numberOfPerson = systemUnderTest.size();
+        //Then
+        assertThat(actual, is(expected));
+        assertThat(numberOfPerson, is(11));
     }
 
     @Test
     public void shouldSortedByBirthday() {
         //Given
-        Person p1 = new Person("QA", "Zsófia", "Kovacs", Gender.FEMALE, LocalDate.of(1999, 04, 18));
+        Person expected = new Person("QA", "Zsófia", "Kovacs", Gender.FEMALE, LocalDate.of(1999, 04, 18));
         List<Person> systemUnderTest = registry.sortByBirthday();
         //When
         Person actual = systemUnderTest.get(10);
         //Then
-        assertThat(p1.getFirstName(),is(actual.getFirstName()));
+        assertThat(actual, is(expected));
     }
 
     @Test
-    public void shouldReturnTheSumOfTheMaleAge(){
+    public void shouldReturnTheSumOfTheMaleAge() {
         //Given
         int expected = 264;
         //When
         int actual = registry.getCalcSumAgeMale();
         //Then
-        assertThat(expected, is(actual));
+        assertThat(actual, is(expected));
     }
 
     @Test
-    public void shouldReturnTheAverageAgeMales(){
+    public void shouldReturnTheAverageAgeMales() {
         //Given
         DecimalFormat df = new DecimalFormat("#.##");
         double expected = 37.71;
         //When
         double actual = Double.valueOf(df.format(registry.getCalcAvgAgeMale()));
         //Then
-        assertThat(expected, is(actual));
+        assertThat(actual, is(expected));
+        ;
     }
 }
